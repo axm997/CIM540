a68031ad6cbfcc3b34c6267ae9a44d6400c0c29c
Ashley Maass <axm997@miami.edu>
2019-05-08 16:22:18 -0400

    kk

--- notes/final2/readme.md
+++ notes/final2/readme.md
@@ -1,3 +1,14 @@
 Club Bear
 
-Welcome to Club Bear! This website allows users to mix and match their own bear with fun college shirts, backgrounds, music, and more! 
+Welcome to Club Bear! This website allows users to mix and match their own bear with fun college shirts, locations, music, and more!
+
+1. I made Club Bear for users who may remember websites like Webkinz or Club Penguin and want to experience the nostalgia again. Club Bear is also open to anyone, old or young, who enjoys the fun of interactive media and animated characters.
+
+2. My motivation for this project was to recreate Club Penguin college edition as most of the users have grown up and may even be in college. I wanted to bring back a fun memory from my own childhood.
+
+3. My inputs are the start button, outfits changes, locations transports, and the club bear button.  
+4. My outputs are music and pawphone lights.
+
+5. My target audience is all ages who may remember similar websites from their childhood or just want to have fun interacting with an animated bear.
+
+6. My project will be displayed on computers mostly but also is compatible with phones.
--- notes/final2/sketch.js
+++ notes/final2/sketch.js
@@ -1,4 +1,5 @@
-//final
+//Club Bear - Ashley Maass - Creative Coding Final
+//Click start, dress your bear by clicking on his pawphone and trying on his shirts, then transport him to different locations! Move your cursor over the top right red light and  the bottom middle to light up. CLick on the music buttons to play and pause the music! Lastly, have fun and click the club bear logo to play again!
 
 var bearhead;
 var bearbody;
@@ -40,6 +41,19 @@ var dressoptions
 var backgrounds = [];
 var backgroundCounter = 0;
 
+var shirtX = 391;
+var shirtY = 301;
+
+var bearbodyX = 357;
+var bearbodyY = 275;
+
+var bearheadX = 412;
+var bearheadY = 205;
+
+var clubsound;
+var name = "";
+var keycounter = 0;
+
 
 
 function preload(){
@@ -58,11 +72,16 @@ sweatshirtArray[1] = loadImage("assets/sweatshirt2.png");
 sweatshirtArray[2] = loadImage("assets/sweatshirt3.png");
 backgrounds[0] = loadImage("assets/nightclub.jpg");
 backgrounds[1] = loadImage("assets/university.jpg");
+backgrounds[2] = loadImage("assets/coffeeshop.jpg");
 dressoptions = loadImage ("assets/dressoptions.png")
 startpage = loadImage ("assets/startpage.jpg")
 empty = loadImage("assets/empty.png");
 pawphonelight = loadImage("assets/pawphonelight.png");
 pawphonelight2 = loadImage("assets/pawphonelight2.png");
+coffeeshop = loadImage("assets/coffeeshop.jpg");
+happyface = loadImage("assets/happyface.png");
+
+clubsound = loadSound("assets/clubbearnightclubmusic.mp3");
 
 }
 
@@ -89,7 +108,6 @@ shirt = empty;
 
 
 
-
 }
 
 
@@ -102,20 +120,87 @@ background(255);
 if(page == 0){
   image(startpage, 0,0, startpage.width, startpage.height);
   bearhead = empty;
-    bearbody = empty;}
+    bearbody = empty;
+  }
 
-  if(mouseX > 609 && mouseX < (609+203) && mouseY > 478 && mouseY < (478+84))
-  { page = 1;
-//    if(mouseIsPressed == true)
-  //  {image(mainpage, 0,0, mainpage.width, mainpage.height);}
+
+//mainpage with (dress)
+if(page == 4){
+  image(mainpagewith, 0,0, mainpagewith.width, mainpagewith.height);
+  text(name, 100,100);
+
+
+  //shortsleeve
+  if(mouseX > 702 && mouseX < (702 + 113)&& mouseY > 137 && mouseY < (137 + 113)){
+    if(mouseIsPressed == true){
+      shirt = shortsleeveArray[0];
+      shirtX = 391;
+      shirtY = 301;
+    }
+
+  }
+
+  if(mouseX > 824 && mouseX < (824 + 113)&& mouseY > 137 && mouseY < (137 + 113)){
+    if(mouseIsPressed == true){
+      shirt = shortsleeveArray[1];
+      shirtX = 300;
+      shirtY = 301;
+    }
+
+  }
+
+  if(mouseX > 946 && mouseX < (946 + 113)&& mouseY > 137 && mouseY < (137 + 113)){
+    if(mouseIsPressed == true){
+      shirt = shortsleeveArray[2];
+      shirtX = 391;
+      shirtY = 301;
+    }
+
+  }
+  //tanks
+    if(mouseX > 702 && mouseX < (702 + 113)&& mouseY > 258 && mouseY < (258 + 113)){
+      if(mouseIsPressed == true){
+        shirt = tankArray[0];
+      }
+
+    }
+
+    if(mouseX > 824 && mouseX < (824 + 113)&& mouseY > 258 && mouseY < (258 + 113)){
+      if(mouseIsPressed == true){
+        shirt = tankArray[1];
+      }
+
+    }
+
+    if(mouseX > 946 && mouseX < (946 + 113)&& mouseY > 258 && mouseY < (258 + 113)){
+      if(mouseIsPressed == true){
+        shirt = tankArray[2];
+      }
+
+    }
+    //sweatshirts
+    if(mouseX > 702 && mouseX < (702 + 113)&& mouseY > 380 && mouseY < (380 + 113)){
+      if(mouseIsPressed == true){
+        shirt = sweatshirtArray[0];
+      }}
+
+      if(mouseX > 824 && mouseX < (824 + 113)&& mouseY > 380 && mouseY < (380 + 113)){
+        if(mouseIsPressed == true){
+          shirt = sweatshirtArray[1];
+        }}
+
+        if(mouseX > 945 && mouseX < (945 + 113)&& mouseY > 380 && mouseY < (380 + 113)){
+          if(mouseIsPressed == true){
+            shirt = sweatshirtArray[2];
+          }}
 
 
 
 }
 
-//mainpage with (dress)
+//coffeeshop
 if(page == 3){
-  image(mainpagewith, 0,0, mainpagewith.width, mainpagewith.height);}
+  image(coffeeshop, 0,0, coffeeshop.width, coffeeshop.height);}
 
 //dresspage
 if(page == 1){
@@ -128,63 +213,7 @@ if(page == 1){
   }
 
 
-//shortsleeve
-if(mouseX > 702 && mouseX < (702 + 113)&& mouseY > 137 && mouseY < (137 + 113)){
-  if(mouseIsPressed == true){
-    shirt = shortsleeveArray[0];
-  }
 
-}
-
-if(mouseX > 824 && mouseX < (824 + 113)&& mouseY > 137 && mouseY < (137 + 113)){
-  if(mouseIsPressed == true){
-    shirt = shortsleeveArray[1];
-  }
-
-}
-
-if(mouseX > 946 && mouseX < (946 + 113)&& mouseY > 137 && mouseY < (137 + 113)){
-  if(mouseIsPressed == true){
-    shirt = shortsleeveArray[2];
-  }
-
-}
-//tanks
-  if(mouseX > 702 && mouseX < (702 + 113)&& mouseY > 258 && mouseY < (258 + 113)){
-    if(mouseIsPressed == true){
-      shirt = tankArray[0];
-    }
-
-  }
-
-  if(mouseX > 824 && mouseX < (824 + 113)&& mouseY > 258 && mouseY < (258 + 113)){
-    if(mouseIsPressed == true){
-      shirt = tankArray[1];
-    }
-
-  }
-
-  if(mouseX > 946 && mouseX < (946 + 113)&& mouseY > 258 && mouseY < (258 + 113)){
-    if(mouseIsPressed == true){
-      shirt = tankArray[2];
-    }
-
-  }
-  //sweatshirts
-  if(mouseX > 702 && mouseX < (702 + 113)&& mouseY > 380 && mouseY < (380 + 113)){
-    if(mouseIsPressed == true){
-      shirt = sweatshirtArray[0];
-    }}
-
-    if(mouseX > 824 && mouseX < (824 + 113)&& mouseY > 380 && mouseY < (380 + 113)){
-      if(mouseIsPressed == true){
-        shirt = sweatshirtArray[1];
-      }}
-
-      if(mouseX > 945 && mouseX < (945 + 113)&& mouseY > 380 && mouseY < (380 + 113)){
-        if(mouseIsPressed == true){
-          shirt = sweatshirtArray[2];
-        }}
 
   //end of icons
 
@@ -193,8 +222,6 @@ if(mouseX > 946 && mouseX < (946 + 113)&& mouseY > 137 && mouseY < (137 + 113)){
 }
 
 //outside of page 1 and 2 will work on all pages
-
-
 
 
 //pawphonelights
@@ -210,16 +237,56 @@ image(pawphonelight, 102,560, pawphonelight.width, pawphonelight.height);
     }
 
 
+//bear smile
+//if(mouseX > 79 && mouseX < (79 + 35)&& mouseY > 676 && mouseY < (676 + 35)){
 
+//image(happyface, 200,676, happyface.width, happyface.height);}
 
 // build bear
 
-image(bearbodyArray[0],357,275, bearbodyArray[0].width/1.2, bearbodyArray[0].height/1.2);
+if(page != 0){
+  image(bearbodyArray[0],bearbodyX,bearbodyY, bearbodyArray[0].width/1.2, bearbodyArray[0].height/1.2);
+  image(shirt, shirtX,shirtY, shirt.width/4.8, shirt.height/4.8);
+  image(bearheadArray[0], bearheadX,bearheadY, bearheadArray[0].width/2.5, bearheadArray[0].height/2.5);
+}
+
+if(page == 1){
+   bearbodyX = 457;
+   bearbodyY = 275;
+
+   bearheadX = 512;
+   bearheadY = 205;
+
+   shirtX = 500;
+   shirtY = 301;
+
+   //adjust shirtX and shirtY
+}else if(page == 2){
+  bearbodyX = 457;
+  bearbodyY = 275;
+
+  bearheadX = 512;
+  bearheadY = 205;
+
+  shirtX = 500;
+  shirtY = 301;
+
+
+}else if(page == 4){
+  bearbodyX = 357;
+  bearbodyY = 275;
+
+  bearheadX = 412;
+  bearheadY = 205;
+
+  shirtX = 391;
+  shirtY = 301;
+}
+
 
 
 //shirts
 
-image(shirt, 391,301, shortsleeveArray[0].width/4.8, shortsleeveArray[0].height/4.8);
 
 
 // image(shortsleeveArray[0], 391,301, shortsleeveArray[0].width/4.8, shortsleeveArray[0].height/4.8);
@@ -236,7 +303,6 @@ image(shirt, 391,301, shortsleeveArray[0].width/4.8, shortsleeveArray[0].height/
 
 
 
-image(bearheadArray[0], 412,205, bearheadArray[0].width/2.5, bearheadArray[0].height/2.5);
 
 //end build bear
 
@@ -246,23 +312,63 @@ image(bearheadArray[0], 412,205, bearheadArray[0].width/2.5, bearheadArray[0].he
 
 function mousePressed(){
 
+  //clicking start
+
+  //    if(mouseIsPressed == true)
+    //  {image(mainpage, 0,0, mainpage.width, mainpage.height);}
+
+  //click logo back to start page
+  if(page == 0){
+    if(mouseX > 609 && mouseX < (609+203) && mouseY > 478 && mouseY < (478+84))
+    { page = 1;}
+  }
+    if(mouseX > 35 && mouseX < (35+156) && mouseY > 38 && mouseY < (38+65))
+    { page = 0;}
+
   //shirts
   if(mouseX > 114 && mouseX < (171) && mouseY > 365 && mouseY < 408){
-  page = 3;
+  page = 4;
   }
 
   // dress
   if(mouseX > 55 && mouseX < (55 +69) && mouseY > 624 && mouseY < 624+30){
-    page = 3;}
+    page = 4;}
 
 
 //transport
   if(mouseX > 55 && mouseX < (55 +69) && mouseY > 654 && mouseY < 654+25){
     page = 2;
     backgroundCounter = backgroundCounter + 1;
-    if(backgroundCounter == 2){
+    if(backgroundCounter == 3){
       backgroundCounter = 0;
     }
 }
 
+//hitzone on icons
+if(clubsound.isPlaying() != true){
+  //(mouseX > 55 && mouseX < (55 +69) && mouseY > 654 && mouseY < 654+25)
+  clubsound.play();
+}
+
+//clubsound.stop();
+
+}
+
+function keyPressed(){
+
+
+  if(name.length < 20){
+
+
+      if(keyCode != BACKSPACE){
+          name += key;
+          font(Arial,120,400)
+        //  text weight
+      }
+
+  }
+
+  if(keyCode == BACKSPACE){
+    name = name.slice(0, name.length-1);
+  }
 }
